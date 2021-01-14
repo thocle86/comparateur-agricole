23b2adb990cc5345b89efa36477766a37aa71e07
thocle86 <thocle86@gmail.com>
2021-01-14 09:15:50 +0100

    Addition of the location of Comparateur Agricole and a button to refocus the map on it + addition of the readme

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,4 @@
-//Récupération des données
+//Récupération des données de la base
 const mapDiv = document.querySelector('#map');
 const farmer = JSON.parse(mapDiv.dataset.farmer);
 
@@ -55,6 +55,8 @@ let mapNight = L.tileLayer(mapboxUrl,
     }
 );
 
+//Test pour récupérer plusieurs villes et les mettre dans un groupe
+//Cela permet de cocher et décocher leur affichage dans la vue
 let littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.');
 let denver = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.');
 let aurora = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.');
@@ -65,12 +67,23 @@ let cities = L.layerGroup([littleton, denver, aurora, golden]);
 //Création de la carte
 let map = L.map('map',
     {
-        center: [39.73, -104.99],
+        center: [48.474968, 1.5363024], //centrage de la vue sur comparateur agricole à l'ouverture de la carte
         zoom: 10,
         layers: [mapDefault, cities]
     }
 );
 
+//Pointeur sur comparateur agricole
+L.marker([48.4474968, 1.5363024]).addTo(map)
+    .bindPopup('Comparateur<br>Agricole.com')
+    .openPopup();
+
+//Fonction pour recentrer sur comparateur agricole
+comparateurAgricole.addEventListener('click', function() {
+    map.setView([48.4474968, 1.5363024], 10);
+});
+
+//Objet avec les différents calques sélectionnables dans la vue
 let baseMaps = {
     "DEFAUT": mapDefault,
     "OUTDOORS": mapOutdoors,
@@ -78,36 +91,41 @@ let baseMaps = {
     "NIGHT": mapNight
 };
 
+//Objet avec les différentes villes sélectionnables dans la vue
 let overlayMaps = {
     "Cities": cities
 };
 
+//Création du panel de sélection des calques et des villes ou autres
 L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 
 //Pointeur sur le farmer envoyé dans la vue
+/*
 L.marker([farmer[1], farmer[2]]).addTo(map)
     .bindPopup(farmer[0])
     .openPopup();
+*/
 
 /**
  * Fonction de localisation
  */
+myLocate.addEventListener('click', function() {
+    map.locate({setView: true, maxZoom: 16});
 
-map.locate({setView: false, maxZoom: 16});
+    function onLocationFound(e) {
+        var radius = e.accuracy;
 
-function onLocationFound(e) {
-    var radius = e.accuracy;
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("Vous êtes dans un rayon de " + radius + " m de ce point").openPopup();
 
-    L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+        L.circle(e.latlng, radius).addTo(map);
+    }
+    map.on('locationfound', onLocationFound);
 
-    L.circle(e.latlng, radius).addTo(map);
-}
-map.on('locationfound', onLocationFound);
+    function onLocationError(e) {
+        alert(e.message);
+    }
 
-function onLocationError(e) {
-    alert(e.message);
-}
-
-map.on('locationerror', onLocationError);
+    map.on('locationerror', onLocationError);
+});
